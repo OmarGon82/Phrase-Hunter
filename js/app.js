895fa3f274379232eadec63a6097eef0c48540ca
Omar Gonzalez <48702844+OmarGon82@users.noreply.github.com>
2019-06-21 08:19:15 +0900

    added  phrase and game classes

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+const phrase = new Phrase('Life is Like a box of choclates');
+console.log(`Phrase - phrase: ${phrase.phrase}`);
