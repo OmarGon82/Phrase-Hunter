68411b3e1914b91a9977bdfdcf07675619cd2f82
Omar Gonzalez <48702844+OmarGon82@users.noreply.github.com>
2019-07-02 12:35:36 +0900

    removed class and enabled keyboard fater win/lose

--- js/app.js
+++ js/app.js
@@ -8,8 +8,27 @@ document.getElementById("btn__reset").addEventListener('click', function() {
 
 document.getElementById('qwerty').addEventListener('click', function(event) {
     const e = event.target;
-    // console.log(e.classList)
+    
  if(e.className === "key"){
     game.handleInteraction(e)
     } 
 })
+
+
+document.getElementById("btn__reset").addEventListener('click', function() {
+    // const overlay = document.getElementById("overlay");
+    const ul = document.querySelector('ul');
+    const lis = ul.children
+    const keys = document.getElementsByClassName("key");
+    
+    if(overlay.className === "lose" || overlay.className === "win") {
+        for(let i = 0; i < keys.length; i++) {
+            keys[i].classList.remove("wrong", "chosen");
+            keys[i].disabled = false;
+        }
+        // for(let i = 0; i < lis; i++) {
+        //     ul[i].removeChild();
+        // }
+    }
+}) 
+
